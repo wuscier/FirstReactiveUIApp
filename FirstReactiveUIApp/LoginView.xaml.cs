236904be77a0bdaeb5636f893e7ca3a6d46f8f5e
wuxu <wuxu@shengtao.net>
2017-05-12 14:02:56 +0800

    add login demo of reactiveui.

---
+++ FirstReactiveUIApp/LoginView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace FirstReactiveUIApp
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView :IViewFor<LoginViewModel>
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+
+            ViewModel = new LoginViewModel();
+
+            this.WhenActivated(d =>
+            {
+                d(this.Bind(ViewModel, x => x.UserName, x => x.UserNameTextBox.Text));
+                d(this.Bind(ViewModel, x => x.Password, x => x.PasswordTextBox.Text));
+                d(this.BindCommand(ViewModel, x => x.LoginCommand, x => x.LoginButton, nameof(LoginButton.Click)));
+                d(this.BindCommand(ViewModel, x => x.ResetCommand, x => x.ResetButton, nameof(ResetButton.Click)));
+                d(this.BindCommand(ViewModel, x => x.LoadCommand, x => x.LoginWindow, nameof(LoginWindow.Loaded)));
+            });
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+
+            set { ViewModel = (LoginViewModel) value; }
+        }
+
+
+
+        public LoginViewModel ViewModel
+        {
+            get { return (LoginViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(LoginViewModel), typeof(LoginView));
+
+
+    }
+}
---
+++ FirstReactiveUIApp/LoginViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+
+namespace FirstReactiveUIApp
+{
+    public class LoginViewModel : ReactiveObject
+    {
+        private readonly ReactiveCommand<Unit, Unit> _loadCommand;
+         private readonly ReactiveCommand<Unit, Unit> _loginCommand;
+        private readonly ReactiveCommand<Unit, Unit> _resetCommand;
+
+        private string _userName;
+        private string _password;
+
+        public LoginViewModel()
+        {
+            var canLogin = this.WhenAnyValue(
+                x => x.UserName,
+                x => x.Password,
+                (username, password) => !string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password));
+
+            _loginCommand = ReactiveCommand.CreateFromObservable(
+                this.LoginAsync, canLogin);
+
+            _resetCommand = ReactiveCommand.Create(() =>
+            {
+                UserName = null;
+                Password = null;
+            });
+
+            _loadCommand = ReactiveCommand.CreateFromTask(LoadAsync);
+        }
+
+        private async Task<Unit> LoadAsync()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string result = await webClient.DownloadStringTaskAsync("https://github.com/wuscier");
+
+                UserName = result;
+
+                return Unit.Default;
+            }
+        }
+
+        public ReactiveCommand<Unit, Unit> LoginCommand => _loginCommand;
+        public ReactiveCommand<Unit, Unit> ResetCommand => _resetCommand;
+        public ReactiveCommand<Unit, Unit> LoadCommand => _loadCommand;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { this.RaiseAndSetIfChanged(ref _userName, value); }
+        }
+
+
+        public string Password
+        {
+            get { return _password; }
+            set { this.RaiseAndSetIfChanged(ref _password, value); }
+        }
+
+        private IObservable<Unit> LoginAsync() => Observable.Return(new Random().Next(0, 2) == 1)
+            .Delay(TimeSpan.FromSeconds(1))
+            .Do(success =>
+            {
+                if (!success)
+                {
+                    MessageBox.Show("登录失败！");
+                }
+                else
+                {
+                    MessageBox.Show("登录成功！");
+                }
+            })
+            .Select(_ => Unit.Default);
+    }
+}
