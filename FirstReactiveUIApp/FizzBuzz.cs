c4429056e29874ca07689c1b312579dca93a1c21
big cash <1352288123@qq.com>
2017-05-15 23:39:07 +0800

    add basic demos.

---
+++ FirstReactiveUIApp/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstReactiveUIApp
+{
+    public class FizzBuzz
+    {
+        Action<long> fizzBuzz = x =>
+        {
+            Console.WriteLine($"{x} - ");
+
+            if (x % 3 == 0)
+            {
+                Console.WriteLine("fizee ");
+
+            }
+
+            if (x % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+
+            Console.WriteLine();
+        };
+
+        
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+       static Action<long> fizzBuzz = x => {
+            Console.WriteLine($"{x} - ");
+
+            if (x % 3 == 0)
+            {
+                Console.Write("fizee ");
+
+            }
+
+            if (x % 5 == 0)
+            {
+                Console.Write("buzz");
+            }
+
+            Console.WriteLine();
+
+        };
+
+        static void Main(string[] args)
+        {
+
+            var query = from number in Enumerable.Range(1, 5)
+                        select number;
+
+
+            var observableQuery = query.ToObservable();
+
+            observableQuery.Subscribe(Console.WriteLine, () => { Console.WriteLine("I'm Done!!!"); });
+
+
+
+            //Observable.Range(1, 100).TakeLast(10).Skip(5)
+            //    .Subscribe(x => fizzBuzz(x), ex => { Console.WriteLine(ex.Message); }, () => { Console.WriteLine("Done..............."); });
+            //Observable.Range(1, 100)
+            //    .Subscribe(x => fizzBuzz(x), ex => { Console.WriteLine(ex.Message); }, () => { Console.WriteLine("Done..............."); });
+
+            //var f = new Subject<int>();
+
+            //f.Subscribe(x => Console.WriteLine($"-- {x} --"));
+
+            //f.OnNext(1000);
+            //f.OnNext(2000);
+            //f.OnNext(3000);
+
+            Console.ReadLine();
+        }
+    }
+}
